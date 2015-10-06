f6c701df049a3ff1212d61bad6ff66982c8b8e46
Myrto <diakv115@newschool.edu>
2015-10-06 19:27:16 +0300

    final

---
+++ City Block/Assets/screenShaker.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class screenShaker : MonoBehaviour {
+
+	public float shakeAmmount = 2f;
+	public float shakeTime = 1.5f;
+	
+	private bool isShaking = false;
+	private Vector3 originalLocation;
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey (KeyCode.F)) {
+			StartCoroutine("shakeScreen");
+		}
+		if (isShaking) {
+			transform.position = originalLocation + Random.insideUnitSphere * shakeAmmount * Time.deltaTime;
+		}
+	}
+	
+	IEnumerator shakeScreen() {
+		isShaking = true;
+		originalLocation = transform.position;
+		yield return new WaitForSeconds(shakeTime);
+		isShaking = false;
+	}
+}
